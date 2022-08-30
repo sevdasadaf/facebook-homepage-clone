2ca314aa5e6b08984f03e3939599d5ee39448371
sevdasadaf <sevdasadaf24@gmail.com>
2022-08-30 20:14:26 +0400

    partly finished

---
+++ script.js
@@ -0,0 +1,248 @@
+// {* <div class="other-posts">
+// <div class="other-posts-header">
+
+//   <div class="posts-profile">
+//     <div class="posts-profile-image">
+//      <img src="./assests/image/pic.jpg" alt="">
+//    </div>
+
+//    <div class="posts-profile-name">
+//       <div class="posts-profile-name-icons">
+//       <span class="other-person-profile-name">Bella Swan</span>
+//       <span class="other-person-post-details">1h &sdot;   <svg fill="currentColor" viewBox="0 0 16 16" width="1em" height="1em"  title="Shared with Public group"><title>Shared with Public group</title><g fill-rule="evenodd" transform="translate(-448 -544)"><g><path d="M109.5 408.5c0 3.23-2.04 5.983-4.903 7.036l.07-.036c1.167-1 1.814-2.967 2-3.834.214-1 .303-1.3-.5-1.96-.31-.253-.677-.196-1.04-.476-.246-.19-.356-.59-.606-.73-.594-.337-1.107.11-1.954.223a2.666 2.666 0 0 1-1.15-.123c-.007 0-.007 0-.013-.004l-.083-.03c-.164-.082-.077-.206.006-.36h-.006c.086-.17.086-.376-.05-.529-.19-.214-.54-.214-.804-.224-.106-.003-.21 0-.313.004l-.003-.004c-.04 0-.084.004-.124.004h-.037c-.323.007-.666-.034-.893-.314-.263-.353-.29-.733.097-1.09.28-.26.863-.8 1.807-.22.603.37 1.166.667 1.666.5.33-.11.48-.303.094-.87a1.128 1.128 0 0 1-.214-.73c.067-.776.687-.84 1.164-1.2.466-.356.68-.943.546-1.457-.106-.413-.51-.873-1.28-1.01a7.49 7.49 0 0 1 6.524 7.434" transform="translate(354 143.5)"/><path d="M104.107 415.696A7.498 7.498 0 0 1 94.5 408.5a7.48 7.48 0 0 1 3.407-6.283 5.474 5.474 0 0 0-1.653 2.334c-.753 2.217-.217 4.075 2.29 4.075.833 0 1.4.561 1.333 2.375-.013.403.52 1.78 2.45 1.89.7.04 1.184 1.053 1.33 1.74.06.29.127.65.257.97a.174.174 0 0 0 .193.096" transform="translate(354 143.5)"/><path fill-rule="nonzero" d="M110 408.5a8 8 0 1 1-16 0 8 8 0 0 1 16 0zm-1 0a7 7 0 1 0-14 0 7 7 0 0 0 14 0z" transform="translate(354 143.5)"/></g></g></svg>   
+//       </span> 
+//       </div>
+//    </div>
+// </div>
+
+//   <div class="other-posts-options">
+//     <svg fill="currentColor" viewBox="0 0 20 20" width="1em" height="1em" class="b6ax4al1 m4pnbp5e somyomsx ahndzqod gnhxmgs4 dmdr2h6l kgzac55p"><g fill-rule="evenodd" transform="translate(-446 -350)"><path d="M458 360a2 2 0 1 1-4 0 2 2 0 0 1 4 0m6 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0m-12 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0"/></g></svg>
+// </div>
+//  </div>
+
+//  <div class="post-share">
+//   <div class="share-post-contents">
+//      <div class="post-share-text">
+//      <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis reprehenderit nostrum similique, enim dolores quos praesentium alias rem obcaecati reiciendis velit doloremque laboriosam nam accusamus fugit doloribus, repudiandae minima magnam sunt impedit sint provident. Ea quam illum voluptates dolorem iste.</span>
+//      </div>
+
+//      <div class="post-share-image">
+//        <img src="./assests/image/post-imageg.jpg" alt="">
+//      </div>
+//    </div>
+
+
+// <div class="post-share-bottom">
+ 
+//     <div class="total-reviews-icons">
+//       <img src="./assests/image/like-svg.svg" alt="">
+//       <img src="./assests/svg/heart-svg.svg" alt="">
+//       <span>210 others</span>
+//     </div>
+//     <div class="total-reviews-text">
+//       <span>11 comments</span>
+//       <span>20 shares</span>
+//     </div> 
+  
+
+// </div>
+
+ 
+
+// <div class="other-posts-bottom">
+//   <div class="other-posts-bottom-review">
+      
+//     <div class="like like-commet-share-buttons">
+//     <div class="like-svg like-comment-share-svgs">
+//       <i data-visualcompletion="css-img" class="gneimcpu oee9glnz" style="background-image: url(&quot;https://static.xx.fbcdn.net/rsrc.php/v3/yZ/r/2k6UCOtK9nq.png&quot;); background-position: 0px -272px; background-size: auto; width: 18px; height: 18px; background-repeat: no-repeat; display: inline-block;"></i>
+//     </div>  
+//     <span>Like</span>
+    
+//     </div>
+
+//     <div class="comment like-commet-share-buttons">
+      
+//       <div class="comment-svg like-comment-share-svgs">
+//       <i data-visualcompletion="css-img" class="gneimcpu oee9glnz" style="background-image: url(&quot;https://static.xx.fbcdn.net/rsrc.php/v3/yZ/r/2k6UCOtK9nq.png&quot;); background-position: 0px -234px; background-size: auto; width: 18px; height: 18px; background-repeat: no-repeat; display: inline-block;"></i>
+//       </div>
+//       <span>Comment</span>
+//     </div>
+    
+//     <div class="share like-commet-share-buttons">
+//       <div class="share-svg like-comment-share-svgs">
+//       <i data-visualcompletion="css-img" class="gneimcpu oee9glnz" style="background-image: url(&quot;https://static.xx.fbcdn.net/rsrc.php/v3/yZ/r/2k6UCOtK9nq.png&quot;); background-position: 0px -291px; background-size: auto; width: 18px; height: 18px; background-repeat: no-repeat; display: inline-block;"></i>
+//       </div>
+//       <span>Share</span>
+//     </div>
+
+//     <div class="my-profile-review">
+//       <img src="./assests/image/pic.jpg" alt="">
+//       <i data-visualcompletion="css-img" class="gneimcpu oee9glnz" style="background-image: url(&quot;https://static.xx.fbcdn.net/rsrc.php/v3/y0/r/J6HRfSmQmbK.png&quot;); background-position: -158px -191px; background-size: auto; width: 12px; height: 12px; background-repeat: no-repeat; display: inline-block;"></i>
+//     </div>
+
+// </div>
+// </div>
+// </div> */
+
+
+
+
+// </div> }
+
+
+
+
+const myStatusSection = document.getElementById('status-section-java')
+
+window.onload = downloadStatus()
+
+async function downloadStatus () {
+  const address = "status.json"
+
+  try {
+    const dataComputerNotGet = await fetch(address)
+    const dataComputerGet = await dataComputerNotGet.json()
+    
+    for (let i=0; i<dataComputerGet.length; i++) {
+      myStatusSection.innerHTML  += `<div class="other-posts">
+      <div class="other-posts-header">
+  
+        <div class="posts-profile">
+          <div class="posts-profile-image">
+           <img src="${dataComputerGet[i].profile_image}" alt="">
+         </div>
+  
+         <div class="posts-profile-name">
+            <div class="posts-profile-name-icons">
+            <span class="other-person-profile-name"> ${dataComputerGet[i].user_name}</span>
+            <span class="other-person-post-details"> ${dataComputerGet[i].share_date} &sdot;   <svg fill="currentColor" viewBox="0 0 16 16" width="1em" height="1em"  title="Shared with Public group"><title>Shared with Public group</title><g fill-rule="evenodd" transform="translate(-448 -544)"><g><path d="M109.5 408.5c0 3.23-2.04 5.983-4.903 7.036l.07-.036c1.167-1 1.814-2.967 2-3.834.214-1 .303-1.3-.5-1.96-.31-.253-.677-.196-1.04-.476-.246-.19-.356-.59-.606-.73-.594-.337-1.107.11-1.954.223a2.666 2.666 0 0 1-1.15-.123c-.007 0-.007 0-.013-.004l-.083-.03c-.164-.082-.077-.206.006-.36h-.006c.086-.17.086-.376-.05-.529-.19-.214-.54-.214-.804-.224-.106-.003-.21 0-.313.004l-.003-.004c-.04 0-.084.004-.124.004h-.037c-.323.007-.666-.034-.893-.314-.263-.353-.29-.733.097-1.09.28-.26.863-.8 1.807-.22.603.37 1.166.667 1.666.5.33-.11.48-.303.094-.87a1.128 1.128 0 0 1-.214-.73c.067-.776.687-.84 1.164-1.2.466-.356.68-.943.546-1.457-.106-.413-.51-.873-1.28-1.01a7.49 7.49 0 0 1 6.524 7.434" transform="translate(354 143.5)"/><path d="M104.107 415.696A7.498 7.498 0 0 1 94.5 408.5a7.48 7.48 0 0 1 3.407-6.283 5.474 5.474 0 0 0-1.653 2.334c-.753 2.217-.217 4.075 2.29 4.075.833 0 1.4.561 1.333 2.375-.013.403.52 1.78 2.45 1.89.7.04 1.184 1.053 1.33 1.74.06.29.127.65.257.97a.174.174 0 0 0 .193.096" transform="translate(354 143.5)"/><path fill-rule="nonzero" d="M110 408.5a8 8 0 1 1-16 0 8 8 0 0 1 16 0zm-1 0a7 7 0 1 0-14 0 7 7 0 0 0 14 0z" transform="translate(354 143.5)"/></g></g></svg>   
+            </span> 
+            </div>
+         </div>
+      </div>
+  
+        <div class="other-posts-options">
+          <svg fill="currentColor" viewBox="0 0 20 20" width="1em" height="1em" class="b6ax4al1 m4pnbp5e somyomsx ahndzqod gnhxmgs4 dmdr2h6l kgzac55p"><g fill-rule="evenodd" transform="translate(-446 -350)"><path d="M458 360a2 2 0 1 1-4 0 2 2 0 0 1 4 0m6 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0m-12 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0"/></g></svg>
+      </div>
+       </div>
+      
+       <div class="post-share">
+        <div class="share-post-contents">
+           <div class="post-share-text">
+           <span>${dataComputerGet[i].share_post}</span>
+           </div>
+  
+           <div class="post-share-image">
+             <img src="${dataComputerGet[i].image}" alt="">
+           </div>
+         </div>
+      
+  
+      <div class="post-share-bottom">
+      
+          <div class="total-reviews-icons">
+            <img src="./assests/image/like-svg.svg" alt="">
+            <img src="./assests/svg/heart-svg.svg" alt="">
+            <span>${dataComputerGet[i].review_counts}</span>
+          </div>
+          <div class="total-reviews-text">
+            <span>${dataComputerGet[i].comment}</span>
+            <span>${dataComputerGet[i].share}</span>
+          </div> 
+        
+  
+      </div>
+  
+       
+    
+      <div class="other-posts-bottom">
+        <div class="other-posts-bottom-review">
+            
+          <div class="like like-commet-share-buttons">
+          <div class="like-svg like-comment-share-svgs">
+            <i data-visualcompletion="css-img" class="gneimcpu oee9glnz" style="background-image: url(&quot;https://static.xx.fbcdn.net/rsrc.php/v3/yZ/r/2k6UCOtK9nq.png&quot;); background-position: 0px -272px; background-size: auto; width: 18px; height: 18px; background-repeat: no-repeat; display: inline-block;"></i>
+          </div>  
+          <span>Like</span>
+          
+          </div>
+  
+          <div class="comment like-commet-share-buttons">
+            
+            <div class="comment-svg like-comment-share-svgs">
+            <i data-visualcompletion="css-img" class="gneimcpu oee9glnz" style="background-image: url(&quot;https://static.xx.fbcdn.net/rsrc.php/v3/yZ/r/2k6UCOtK9nq.png&quot;); background-position: 0px -234px; background-size: auto; width: 18px; height: 18px; background-repeat: no-repeat; display: inline-block;"></i>
+            </div>
+            <span>Comment</span>
+          </div>
+          
+          <div class="share like-commet-share-buttons">
+            <div class="share-svg like-comment-share-svgs">
+            <i data-visualcompletion="css-img" class="gneimcpu oee9glnz" style="background-image: url(&quot;https://static.xx.fbcdn.net/rsrc.php/v3/yZ/r/2k6UCOtK9nq.png&quot;); background-position: 0px -291px; background-size: auto; width: 18px; height: 18px; background-repeat: no-repeat; display: inline-block;"></i>
+            </div>
+            <span>Share</span>
+          </div>
+  
+          <div class="my-profile-review">
+            <img src="./assests/image/pic.jpg" alt="">
+            <i data-visualcompletion="css-img" class="gneimcpu oee9glnz" style="background-image: url(&quot;https://static.xx.fbcdn.net/rsrc.php/v3/y0/r/J6HRfSmQmbK.png&quot;); background-position: -158px -191px; background-size: auto; width: 12px; height: 12px; background-repeat: no-repeat; display: inline-block;"></i>
+          </div>
+  
+    </div>
+      </div>
+    </div>
+  
+  
+  
+  
+   </div> `
+    }
+
+
+
+  }
+
+
+
+  catch (err) {
+
+  }
+
+
+
+}
+
+
+const myContactsSection = document.getElementById('my-contact-section')
+
+window.onload = downloadContacts()
+
+async function downloadContacts ()  {
+  
+  const contactsFiles = "contacts.json"
+   
+  try {
+
+    const dataDeviceNotGet = await fetch(contactsFiles)
+    const dataDeviceGet = await dataDeviceNotGet.json()
+    
+    for(let i=0; i<dataDeviceGet.length; i++) {
+      
+      myContactsSection.innerHTML += `
+      <div class="contacts-person">
+      <img src="${dataDeviceGet[i].profiles_image}" alt="">
+      <div class="online-sign"></div>
+      <span>${dataDeviceGet[i].users_name}</span>
+      </div>`
+
+    }
+
+  }
+
+
+
+
+
+
+  catch {}
+  
+
+}
